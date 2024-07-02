625b7330b4017158c48e9cc491fdac19dff241d2
Roxy <155686159+RoxyKeatings@users.noreply.github.com>
2024-07-02 13:30:40 -0400

    imported bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>GoTemp Weather APP</h1>
-      <footer>
-        THIS PROJECT WAS CODED BY{" "}
-        <a
-          href="https://roxannekeatings.netlify.app"
-          target="_blank"
-          rel="noreferrer">
-          ROXANNE KEATINGS, AND IS OPEN-SOURCED ON{" "}
+      <div className="container">
+        <h1>GoTemp Weather APP</h1>
+        <footer>
+          THIS PROJECT WAS CODED BY{" "}
+          <a
+            href="https://roxannekeatings.netlify.app"
+            target="_blank"
+            rel="noreferrer">
+            ROXANNE KEATINGS, AND IS OPEN-SOURCED ON{" "}
+          </a>
           <a
             href="https://github.com/RoxyKeatings/GoTemp-weather-app"
             target="_blank"
             rel="noreferrer">
             GITHUB
+          </a>{" "}
+          AND IS HOSTED ON{" "}
+          <a
+            href="https://gotemp.netlify.app/"
+            target="_blank"
+            rel="noreferrer">
+            NETLIFY
           </a>
-        </a>
-      </footer>
+        </footer>
+      </div>
     </div>
   );
 }
