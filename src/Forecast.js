37ecd462c87a678dce18970b3d8a6de5cf232873
Roxy <155686159+RoxyKeatings@users.noreply.github.com>
2024-07-06 15:06:16 -0400

    left forecast div empty temporarily

--- src/Forecast.js
+++ src/Forecast.js
@@ -4,12 +4,12 @@ export default function Forecast() {
     <div className="Forecast m-5">
       <div className="container">
         <div className="row">
-          <div className="col-2">☀️</div>
-          <div className="col-2">☀️</div>
-          <div className="col-2">☀️</div>
-          <div className="col-2">☀️</div>
-          <div className="col-2">☀️</div>
-          <div className="col-2">☀️</div>
+          <div className="col-2"></div>
+          <div className="col-2"></div>
+          <div className="col-2"></div>
+          <div className="col-2"></div>
+          <div className="col-2"></div>
+          <div className="col-2"></div>
         </div>
       </div>
     </div>
